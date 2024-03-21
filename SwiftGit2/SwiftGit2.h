6606bdcd2738eca3ee15aa317200548551e8a024
codewithyou365 <codewithyou365@gmail.com>
2024-03-21 18:53:10 +0800

    avoid xcode error: 'Error (Xcode): use of '@import' in framework header is discouraged, including this header requires -fmodules'

--- SwiftGit2/SwiftGit2.h
+++ SwiftGit2/SwiftGit2.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 GitHub, Inc. All rights reserved.
 //
 
-@import Foundation;
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftGit2.
 FOUNDATION_EXPORT double SwiftGit2VersionNumber;
